ad2960218054be53727438331c27b83212117aab
Adityaexe16 <adityaexe16@gmail.com>
2024-06-24 23:05:31 +0530

    updata error in code

--- main.java
+++ main.java
@@ -7,12 +7,12 @@ public class main {
 
 //        First project stone paper sicssor game
         System.out.println("Enter number 0 for stone , 1 for paper , 2 for sicssor");
-        Scanner sc=new Scanner (Systen.in);
-        int userInput=sc.nextInt;
+        Scanner sc=new Scanner (System.in);
+        int userInput=sc.nextInt();
 
         Random ra=new Random ();
-        int computerInput= random.nextInt(3);
-        
+        int computerInput= ra.nextInt(3);
+
         if (userInput==computerInput){
             System.out.println("Draw");
         } else if ( userInput==0&&computerInput==1 || userInput==1&&computerInput==2 ||userInput==2&&computerInput==0) {
@@ -21,6 +21,7 @@ public class main {
         else {
             System.out.println("you win");
         }
-        System.out.println("Computer chooise"+computerInput);
+        System.out.println("Computer chooise  " +computerInput);
+
     }
 }

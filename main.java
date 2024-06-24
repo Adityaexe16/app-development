2108f5bf666d5bfdf4d9ad1c8e7e099bcc77eb0e
Adityaexe16 <adityaexe16@gmail.com>
2024-06-24 21:23:15 +0530

    first project game

--- main.java
+++ main.java
@@ -1,6 +1,26 @@
 package com.company;
+import java.util.Scanner;
+import java.util.Random
 public class main {
     public static void(public static void main(String[]args) {
         System.out.println("jai shree Ram");
+
+//        First project stone paper sicssor game
+        System.out.println("Enter number 0 for stone , 1 for paper , 2 for sicssor");
+        Scanner sc=new Scanner (Systen.in);
+        int userInput=sc.nextInt;
+
+        Random ra=new Random ();
+        int computerInput= random.nextInt(3);
+        
+        if (userInput==computerInput){
+            System.out.println("Draw");
+        } else if ( userInput==0&&computerInput==1 || userInput==1&&computerInput==2 ||userInput==2&&computerInput==0) {
+            System.out.println("computer Win");
+        }
+        else {
+            System.out.println("you win");
+        }
+        System.out.println("Computer chooise"+computerInput);
     }
 }
